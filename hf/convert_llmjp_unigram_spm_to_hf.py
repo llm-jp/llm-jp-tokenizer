9c509ecce3f44c2d5c548fd52b0bcdf6d11ad7e0
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2023-09-10 10:52:54 +0300

    huggingface fast tokenizer

---
+++ hf/convert_llmjp_unigram_spm_to_hf.py
@@ -0,0 +1,123 @@
+import argparse
+
+from tokenizers import decoders, models, normalizers, pre_tokenizers, Regex, Tokenizer
+
+
+"""Tokenizer convert tool for llmjp-tokenizer.
+
+You need to install some packages beforehand.
+
+```console
+$ pip -r requirements.txt
+```
+
+And retain `sentencepiece_model_pb2.py` from official site.
+
+```console
+$ curl -O https://raw.githubusercontent.com/google/sentencepiece/master/python/sentencepiece_model_pb2.py
+```
+
+Then you can convert sentence piece model file to huggingface fast tokenizer file (tokenizer.json).
+
+```console
+$ python convert_llmjp_unigram_spm_to_hf.py -i ver2/code10k_en20k_ja30k.ver2.1.model -o tokenizer.json
+```
+
+After the conversion, you can create fast tokenizer directory from `tokenizer.json`.
+
+```python
+from tokenizers import Tokenizer
+
+tokenizer = Tokenizer.from_file(tokenizer_json_path)
+```
+
+We're still trying to create tokenizer instance via `AutoTokenizer.from_pretrained()`. Wait a moment.
+
+
+This script originates from the following tokenizers codes:
+
+https://github.com/huggingface/tokenizers/blob/v0.14.0/bindings/python/py_src/tokenizers/implementations/sentencepiece_unigram.py
+https://github.com/huggingface/tokenizers/blob/v0.14.0/bindings/python/scripts/convert.py
+"""
+
+def get_proto():
+    try:
+        import sys
+
+        sys.path.append(".")
+
+        import sentencepiece_model_pb2 as model
+    except Exception:
+        raise Exception(
+            "You don't seem to have the required protobuf file, in order to use this function you need to run `pip install protobuf` and `wget https://raw.githubusercontent.com/google/sentencepiece/master/python/sentencepiece_model_pb2.py` for us to be able to read the intrinsics of your spm_file. `pip install sentencepiece` is not required."
+        )
+
+    m = model.ModelProto()
+    return m
+
+
+def convert_llmjp_unigram_spm_to_hf(input_sp_model_path) -> Tokenizer:
+    proto = get_proto()
+    proto.ParseFromString(open(input_sp_model_path, "rb").read())
+    model_type = proto.trainer_spec.model_type
+    vocab = [(piece.piece, piece.score) for piece in proto.pieces]
+    unk_id = proto.trainer_spec.unk_id
+    assert model_type == 1, f"You're trying to run a `Unigram` model but you're file was trained with a different algorithm ({model_type=})"
+    tokenizer = Tokenizer(models.Unigram(vocab, unk_id))
+    normalizer_list = []
+    precompiled_charsmap = proto.normalizer_spec.precompiled_charsmap
+    if precompiled_charsmap:
+        normalizer_list.append(normalizers.Precompiled(precompiled_charsmap))
+    replacement = "▁"
+    """
+    # do not use Metaspace pre_tokenizer because all the continuous spaces are devided into sigle space sequences 
+    tokenizer.pre_tokenizer = pre_tokenizers.Metaspace(
+        replacement=replacement, add_prefix_space=True
+    )
+    """
+    # using normalizer to insert "▁" to the beggining of text and to replace space to "▁"
+    tokenizer.normalizer = normalizers.Sequence(
+        [
+            normalizers.Replace(Regex("^"), replacement),
+            normalizers.Replace(Regex(" "), replacement),
+        ]
+    )
+    """
+    # do not use Metaspace decoder because all the heading spaces are removed
+    tokenizer.decoder = decoders.Metaspace(
+        replacement=replacement, add_prefix_space=True
+    )
+    """
+    # using Replace decoders to remove the extra space char at the beggining of text and replace "▁" to space
+    tokenizer.decoder = decoders.Sequence(
+        [
+            decoders.Replace(Regex(replacement), " "),
+            decoders.Fuse(),
+            decoders.Replace(Regex(f"^ "), ""),
+        ]
+    )
+    return tokenizer
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input_sp_model_path",
+        required=True,
+        type=str,
+        help="path for input sentencepiece unigram model file",
+    )
+    parser.add_argument(
+        "-o", "--output_hf_tokenizer_json_path",
+        required=True,
+        type=str,
+        help="path for output huggingface tokenizers json file",
+    )
+    args = parser.parse_args()
+    print("converting", args.input_sp_model_path, "to" ,args.output_hf_tokenizer_json_path)
+    tokenizer = convert_llmjp_unigram_spm_to_hf(args.input_sp_model_path)
+    tokenizer.save(args.output_hf_tokenizer_json_path)
+
+
+if __name__ == "__main__":
+    main()
