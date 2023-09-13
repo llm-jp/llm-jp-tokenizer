52c5cc9880c11a73e5bd4c4ab23c1e413a96cb1f
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2023-09-13 03:48:07 +0200

    rename and take files

--- hf/test_spm_hf.py
+++ hf/test_spm_hf.py
@@ -1,10 +1,14 @@
 import sys
+from typing import List, Optional
 
-import sentencepiece as spm
+from sentencepiece import SentencePieceProcessor
 from tokenizers import Tokenizer
 
 
-def print_results(sp, hf, text: str):
+USAGE = f"""Usage: python test_spm_hf.py sentencepiece_model_file huggingface_tokenizer_json [input_files]"""
+
+
+def print_results(sp: SentencePieceProcessor, hf: Tokenizer, text: str):
     print("input text", f'"{text}"')
     encorded_sp = sp.encode(text, out_type="immutable_proto")
     encorded_hf = hf.encode(text)
@@ -29,23 +33,24 @@ def print_results(sp, hf, text: str):
     print()
 
 
-def main(spm_path: str, hf_json_path: str):
-    sp = spm.SentencePieceProcessor(spm_path)
+def main(spm_path: str, hf_json_path: str, target_files: Optional[List[str]]=None):
+    sp = SentencePieceProcessor(spm_path)
     hf = Tokenizer.from_file(hf_json_path)
 
-    test_strings = [
-        "  これはテストです。  これもテストです。  ",
-        "  this is x  this is x  ",
-        "  <unk>  <s>  </s>  <mask>  <pad>  <CLS>  <SEP>  <EOD>  "
-    ]
-
-    for text in test_strings:
-        print_results(sp, hf, text)
-
-    while True:
-        text = input()
-        print_results(sp, hf, text)
+    if target_files:
+        for file in target_files:
+            with open(file, "r", encoding="utf8") as fin:
+                for line in fin:
+                    print_results(sp, hf, line.rstrip("\n"))
+    else:
+        test_strings = [
+            "  これはテストです。  これもテストです。  ",
+            "  this is x  this is x  ",
+            "  <unk>  <s>  </s>  <mask>  <pad>  <CLS>  <SEP>  <EOD>  "
+        ]
+        for text in test_strings:
+            print_results(sp, hf, text)
 
 
 if __name__ == "__main__":
-    main(spm_path=sys.argv[1], hf_json_path=sys.argv[2])
+    main(*sys.argv[1:])
